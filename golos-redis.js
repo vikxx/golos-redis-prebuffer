7f73db5e2406bea1f1e33c0857322fed9b91728a
vik <v.lateralus@gmail.com>
2017-09-22 23:05:34 +0300

    Update golos-redis.js

--- golos-redis.js
+++ golos-redis.js
@@ -1,13 +1,17 @@
-require('events').EventEmitter.prototype._maxListeners = 100000;
+require('events').EventEmitter.prototype._maxListeners = 10000;
 const Promise = require("bluebird")
 const _ = require('lodash')
 const golos = require("golos-js")
 const redis = require("redis")
 const client = redis.createClient()
-golos.config.set('websocket','ws://localhost:9090')
+//golos.config.set('websocket','wss://ws.golos.io')
+golos.config.set('websocket','ws://localhost:9090');
 golos.config.set('address_prefix','GLS')
 golos.config.set('chain_id','782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12')
-let trig = {existBlock:true}
+let fixBlock = true
+let isNext = 0;
+let EmptyJson = JSON.stringify({"timestamp":"0000-00-00T00:00:00"})
+				
 const dynamicSnap = new Promise((resolve, reject) => {
     golos.api.getDynamicGlobalProperties((err, res) => {
         if (err) {console.log(err)}
@@ -19,31 +23,46 @@ const dynamicSnap = new Promise((resolve, reject) => {
 const FIRSTBLOCK = n => n.head_block_number
 const SENDBLOCK = currentblock => {
     golos.api.getBlock(currentblock, (err, result) => {
-		if (err) {
-			console.log(err) 
-			} 	else if (result === null){
-				trig.existBlock = false
-				}else {
-		let JSONblock = JSON.stringify(result)
-		client.hmset("GolosLastBlock", "data",JSONblock);
-		console.log(JSONblock)
-		trig.existBlock = true
+		
+		if (err || result === null) {
+			console.warn(err) 
+			client.hmset("GolosLastBlock", "data",EmptyJson);
+			fixBlock = false
 			}
+			else
+			{
+				
+		       if(result.previous !== isNext){
+				   let JSONblock = JSON.stringify(result)
+		console.log(`========================${result.timestamp}=========================
+${JSONblock}`)
+		fixBlock = true
+		isNext = result.previous
+		return client.hmset("GolosLastBlock", "data",JSONblock);
+			   }else{
+				   console.warn(`ОШИБКА result.previus ${result.previous}`)
+			   }
+
+		
+		}
 	})
 }
 const NEXTBLOCKS = firstblock => {
-    let currentblock = firstblock
+    let currentblock = firstblock 
+	
     setInterval(() => {
-if(trig.existBlock){
+if(fixBlock){
 					currentblock++
-				}else{console.warn(`Проблема блока ${currentblock}`)}
+				}else{
+					console.warn(`Проблема блока ${currentblock}`)
+					client.hmset("GolosLastBlock", "data",EmptyJson);
+				}
 		SENDBLOCK(currentblock)
-	//	trig.existBlock = true
-    }, 3000)
+
+    }, 3000);
+	
 }
-dynamicSnap
-    .then(FIRSTBLOCK)
-    .then(NEXTBLOCKS)
-    .catch(e => console.log(e));
+
+dynamicSnap.then(FIRSTBLOCK).then(NEXTBLOCKS).catch(e => console.log(e));
 	
 	
